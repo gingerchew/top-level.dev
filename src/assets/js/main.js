27ed25fe1af337a81f0232dd5de54de541202d99
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-08 22:49:58 -0400

    Code to handle search

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,31 @@
 //import '/assets/css/main.scss'
 
 // Focus Visible Polyfill
-import 'focus-visible'
+// import 'focus-visible'
 
 // Internal Modules
-import './modules/nav'
+// import './modules/nav'
+
+import { createApp } from 'https://esm.sh/petite-vue';
+
+function Domains() {
+    return {
+        $template: `<li v-for="domain of domains">.{{domain}}</li>`,
+    }
+}
+createApp({
+    _domains: JSON.parse($data.innerHTML.trim()),
+    /**
+     * @param {string} val
+     */
+    _search: '',
+    get domains() {
+        // console.log(this._domains)
+        // if (this._search === '') return this._domains;
+        return this._domains.filter(d => d.toLowerCase().indexOf(this._search) > -1);
+    },
+    search({ target }) {
+        this._search = target.value;
+    },
+    Domains
+}).mount();
