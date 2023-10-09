57346c6633cd8cc2a7e4fbaf51b0aa0751620663
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-08 23:43:39 -0400

    Adds pagination
    this should help with performance issues since less dom
    needs to be rendered at once

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'petite-vue';
+import { juhla } from 'juhla'
+
+const _ = juhla();
+function Pagination() {
+    const _alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    return {
+        $template: `<li v-for="alpha of _alpha">
+            <button type="button" @click="navigate">{{ alpha }}</button>
+        </li>`,
+        _alpha,
+        navigate({ target }) {
+            const alpha = target.textContent;
+
+            this._activeDomains = alpha;
+        }
+    }
+}
 
 function Domains() {
     return {
@@ -6,16 +23,21 @@ function Domains() {
     }
 }
 createApp({
+    _activeDomains: 'a',
     _domains: JSON.parse($data.innerHTML.trim()),
     /**
      * @param {string} val
      */
     _search: '',
     get domains() {
+        if (this._search === '') {
+            return this._domains.filter(d => d[0] === this._activeDomains);
+        }
         return this._domains.filter(d => d.toLowerCase().indexOf(this._search) > -1);
     },
     search({ target }) {
         this._search = target.value;
     },
-    Domains
+    Domains,
+    Pagination,
 }).mount();
